a9c3f7ddbf396d323183b89f43a49739c4714504
Juan Palomino <N00336713@upn.pe>
2025-04-04 00:48:09 -0500

    Operaciones basicas

---
+++ src/Semana01/Operacaciones.java
@@ -0,0 +1,19 @@
+
+package Semana01;
+
+public class Operacaciones {
+    public static void main(String[] args) {
+        //Declarar variables
+        int Num1 = 20;
+        int Num2 = 16;
+        
+        // Realizar operaciones
+        
+        System.out.println("El resultado de la Suma es: "+(Num1 + Num2));
+        System.out.println("El resultado de la Resta es: "+(Num1 - Num2));
+        System.out.println("El resultado de la Multiplicacion es: "+(Num1 * Num2));
+        System.out.println("El resultado de la Division es: "+(Num1 / Num2));
+        System.out.println("El resultado de la Potencia es: "+Math.pow(Num1,Num2));
+    }
+    
+}
