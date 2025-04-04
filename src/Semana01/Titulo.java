16e265bdadddecc2a7f4d47db944e4b06926fb84
Juan Palomino <N00336713@upn.pe>
2025-04-04 00:17:35 -0500

    Conociminetos basicos de Java

---
+++ src/Semana01/Titulo.java
@@ -0,0 +1,10 @@
+package Semana01;
+
+public class Titulo {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Las Operaciones Basicas");
+    }
+    
+}
