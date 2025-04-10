f5cae073441104bf48e8105f6d65905a4754d328
Gianfranco <n00398630@upn.pe>
2025-04-09 21:41:28 -0500

    Se comento el codigo de resta

--- src/Semana01/Operacaciones.java
+++ src/Semana01/Operacaciones.java
@@ -10,7 +10,7 @@ public class Operacaciones {
         // Realizar operaciones
         
         System.out.println("El resultado de la Suma es: "+(Num1 + Num2));
-        System.out.println("El resultado de la Resta es: "+(Num1 - Num2));
+        //System.out.println("El resultado de la Resta es: "+(Num1 - Num2));
         System.out.println("El resultado de la Multiplicacion es: "+(Num1 * Num2));
         System.out.println("El resultado de la Division es: "+(Num1 / Num2));
         System.out.println("El resultado de la Potencia es: "+Math.pow(Num1,Num2));
